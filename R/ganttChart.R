ed1dc84e5e814f9b856d39e586702b254d3f4e3e
David Studer <davidstuder@Davids-MacBook-Pro.local>
2020-11-21 12:18:56 +0100

    stable version

---
+++ R/ganttChart.R
@@ -0,0 +1,31 @@
+ganttChart <- function(excel_file, title="Gantt-Chart", point_size=5, date_breaks = "4 weeks", minor_breaks = "1 week", save = FALSE){
+
+  assertthat::assert_that(file.exists(here::here(excel_file)))
+
+  df <- df %>% mutate(from = as.Date(from),
+                      to = as.Date(to),
+                      group = factor(group, levels = unique(df$group))
+  ) %>%
+    arrange(desc(group), desc(from)) %>%
+    #arrange(desc(from)) %>%
+    mutate(task = forcats::fct_inorder(task)) %>%
+    pivot_longer(cols = c(from, to), values_to = "date") %>%
+    mutate(pch = ifelse(status %in% c("beendet", "abgeschlossen") & name == "to", 15, 18),
+           pch = ifelse(name == 'from', 32, pch))  # Am Anfang der Linie kein Punkt
+
+
+  ggplot(data = df, aes(x=date, y=task, colour = group)) +
+    geom_line(lwd=3, alpha=.7) +
+    scale_shape_identity(aes(fill=group)) +
+    geom_point(aes(group=group, shape=pch), size=point_size) +
+    scale_x_date(position="top", date_breaks = date_breaks, minor_breaks = minor_breaks,
+                 date_labels="%m/%y") +
+    theme_minimal() +
+    theme(
+      legend.position = "bottom",
+      plot.title = element_text(size=22)
+    ) +
+    ggtitle(title) +
+    labs(y="", x = "", colour="Bereich") +
+    ggsave("gantt-Diagramm.png", units = "cm", height = 17, width=32)
+}
